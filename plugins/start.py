6e39d20973da6080500f05638ff4ec10763c88c4
Phoenixhacker76 <78677838+Phoenixhacker76@users.noreply.github.com>
2024-07-22 16:39:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -87,8 +87,8 @@ async def start_command(client: Client, message: Message):
             [
     
     [
-                    InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/Team_Netflix/40')
+                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Close", callback_data = "close")
         
     ]
             ]
